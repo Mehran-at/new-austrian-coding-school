4c88693746ef85c9266b4c8216383f5578251848
Norbert Molnár <43742546+norbix93@users.noreply.github.com>
2019-02-21 17:01:29 +0100

    Add files via upload

---
+++ src/module/Bean.java
@@ -0,0 +1,10 @@
+package module;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
+public class Bean {
+    private String colour;
+}
---
+++ src/view/Screen.java
@@ -0,0 +1,29 @@
+package view;
+
+import controller.OverallExperiment;
+import controller.Statistician;
+import lombok.experimental.UtilityClass;
+import module.Experiment;
+
+import java.util.List;
+
+@UtilityClass
+public class Screen {
+
+    public void display() {
+        Integer numberOfExperiments = 5;
+        List<Experiment> experiments = OverallExperiment.perform(numberOfExperiments);
+        experiments.stream()
+                .forEach(experiment -> System.out.println(experiment));
+
+        System.out.println("Total number of black beans: " +
+                Statistician.getTotalNumberOfBlackBeans(experiments));
+        System.out.println("Total number of white beans: " +
+                Statistician.getTotalNumberOfWhiteBeans(experiments));
+        System.out.println("Total number of black beans as last: " +
+                Statistician.getTotalNumberOfBlackAsLast(experiments));
+        System.out.println("Total number of white beans as last: " +
+                Statistician.getTotalNumberOfWhiteAsLast(experiments));
+        System.out.println("Total number of experiments: " + numberOfExperiments);
+    }
+}
